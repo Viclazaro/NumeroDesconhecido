d98bbf93d4eaae0fc8658eb80aaf5d8a6883e23b
Yen <thearyazitah@gmail.com>
2025-12-16 21:14:26 -0300

    start

---
+++ app.js
@@ -0,0 +1,12 @@
+alert('Partiu jogar?'); //inicio
+
+let SecretNumber = 8; //declarar a variável
+console.log('Número secreto é', SecretNumber) //somente para desenvolvedor
+let answer= prompt('Digite seu número entre 1 e 10'); //solicitar o número ao usuário
+
+if(SecretNumber == answer){ //condição
+    alert(`Acertô mizeravi! Numeruzao é ${SecretNumber}`);
+ } else {
+    alert('errou vei');
+ }
+ 
