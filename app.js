e060b1a40a490ab992ae8f350746fc89724bb1b2
Yen <thearyazitah@gmail.com>
2025-12-30 09:49:29 -0300

    fix

--- app.js
+++ app.js
@@ -4,9 +4,11 @@ let SecretNumber = 8; //declarar a variável
 console.log('Número secreto é', SecretNumber) //somente para desenvolvedor
 let answer= prompt('Digite seu número entre 1 e 10'); //solicitar o número ao usuário
 
+// se chute for igual ao número secreto
 if(SecretNumber == answer){ //condição
     alert(`Acertô mizeravi! Numeruzao é ${SecretNumber}`);
  } else {
     alert('errou vei');
  }
  
+
